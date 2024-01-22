aa2bf4b01bf257294064706aad8892b41937779a
Dongmin Yu <ydm2790@gmail.com>
2024-01-23 01:19:09 +0900

    chore: day 1

---
+++ lib/flutter_boot_plus.dart
@@ -0,0 +1,156 @@
+/// This example code can be run immediately
+/// via the dartpad at the following address.
+/// Author: AndrewDongminYoo<ydm2790@gmail.com>
+/// https://dartpad.dev/?id=7f52e59b9167457fa5ff80763cc20a5a
+
+// 🐦 Flutter imports:
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// 📦 Package imports:
+import 'package:google_fonts/google_fonts.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'FlutterBoot Plus',
+      home: FlutterBootPlus(),
+    );
+  }
+}
+
+class FlutterBootPlus extends StatefulWidget {
+  const FlutterBootPlus({Key? key}) : super(key: key);
+
+  @override
+  State<FlutterBootPlus> createState() => _FlutterBootPlusState();
+}
+
+class _FlutterBootPlusState extends State<FlutterBootPlus> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(15),
+        width: MediaQuery.sizeOf(context).width,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const SizedBox(height: 35),
+            SizedBox(
+              width: MediaQuery.sizeOf(context).width,
+              child: Text(
+                'FlutterBoot Plus',
+                textAlign: TextAlign.start,
+                style: GoogleFonts.roboto(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Feature(
+              icon: CupertinoIcons.bolt_fill,
+              title: 'Quickstart templates',
+              content:
+                  'You can deploy an AI app in seconds on Vercel, using our pre-built templates.',
+            ),
+            const Feature(
+              icon: CupertinoIcons.flame_fill,
+              title: 'Optimized compute',
+              content:
+                  'Deliver globally performant apps without additional infrastructure complexity.',
+            ),
+            const Feature(
+              icon: Icons.speed,
+              title: 'Zero-config streaming',
+              content:
+                  'Easily stream long-running LLM responses for a better user experience.',
+            ),
+            const Spacer(),
+            const Text(
+              'Restore subscription',
+              style: TextStyle(fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(height: 15),
+            const Text(r'Auto-renews for $25/month until canceled'),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                fixedSize: Size(MediaQuery.sizeOf(context).width, 35),
+                splashFactory: NoSplash.splashFactory,
+                visualDensity:
+                    const VisualDensity(horizontal: -4, vertical: -4),
+              ),
+              onPressed: () {},
+              child: const Text(
+                'Subscribe',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Feature extends StatelessWidget {
+  const Feature({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.content,
+  });
+
+  final IconData icon;
+  final String title;
+  final String content;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        children: [
+          IconButton(
+            icon: Icon(icon),
+            iconSize: 30,
+            visualDensity: const VisualDensity(horizontal: -4, vertical: -4),
+            onPressed: () {},
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  content,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/my_chat_gpt.dart
@@ -0,0 +1,171 @@
+/// This example code can be run immediately
+/// via the dartpad at the following address.
+/// Author: AndrewDongminYoo<ydm2790@gmail.com>
+/// https://dartpad.dev/?id=a748246508b1af2fd6007b0f0c05d31b
+
+// 🐦 Flutter imports:
+import 'package:flutter/material.dart';
+
+const profile = 'https://avatars.githubusercontent.com/u/82999715?v=4';
+const chatGpt =
+    'https://cdn.oaistatic.com/_next/static/media/favicon-32x32.be48395e.png';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application.
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF10A37F)),
+        useMaterial3: true,
+      ),
+      home: const MyChatGPT(),
+    );
+  }
+}
+
+class MyChatGPT extends StatefulWidget {
+  const MyChatGPT({Key? key}) : super(key: key);
+
+  @override
+  State<MyChatGPT> createState() => _MyChatGPTState();
+}
+
+class _MyChatGPTState extends State<MyChatGPT> {
+  List<ChatMessage> messages = [];
+  final controller = TextEditingController();
+  final focusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    messages = [ChatMessage(content: 'Hello, how can I help you?')];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: RichText(
+          text: const TextSpan(
+            children: [
+              TextSpan(
+                text: 'MyChatGPT',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              WidgetSpan(child: SizedBox(width: 10)),
+              TextSpan(
+                text: '3.5',
+                style: TextStyle(fontSize: 14, color: Colors.grey),
+              )
+            ],
+          ),
+        ),
+        leading: const Icon(Icons.menu),
+        actions: [
+          IconButton(icon: const Icon(Icons.edit), onPressed: () {}),
+          IconButton(icon: const Icon(Icons.more_vert), onPressed: () {}),
+        ],
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(15),
+        child: Column(children: messages.map(buildChatMessage).toList()),
+      ),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10),
+            width: MediaQuery.sizeOf(context).width,
+            child: TextFormField(
+              focusNode: focusNode,
+              controller: controller,
+              decoration: const InputDecoration(
+                hintText: 'Message',
+                filled: true,
+                iconColor: Color(0xFF49454E),
+                fillColor: Color(0xFFE7E2E9),
+                suffixIcon: Icon(Icons.keyboard_voice),
+                suffixIconConstraints: BoxConstraints(minWidth: 50),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+              onFieldSubmitted: (value) {
+                messages.add(ChatMessage(
+                  isMine: true,
+                  name: 'AndrewDongminYoo',
+                  content: value,
+                ));
+                messages.add(ChatMessage());
+                controller.clear();
+                focusNode.requestFocus();
+                setState(() {});
+              },
+            ),
+          ),
+          Container(
+            width: 258,
+            margin: const EdgeInsets.only(bottom: 10),
+            child: const Text(
+              'ChatGPT can make mistakes. Consider checking important information.',
+              maxLines: 2,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildChatMessage(ChatMessage message) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CircleAvatar(
+            backgroundImage: message.isMine
+                ? const NetworkImage(profile)
+                : const NetworkImage(chatGpt),
+          ),
+          const SizedBox(width: 10),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(message.name),
+              SizedBox(
+                width: MediaQuery.sizeOf(context).width - 90,
+                child: Text(
+                  message.content,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 30,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ChatMessage {
+  ChatMessage({
+    this.isMine = false,
+    this.name = 'My GPT',
+    this.content =
+        "Actually, I don't have any features, but one day I'll grow up and become ChatGPT!",
+  });
+
+  bool isMine;
+  String name;
+  String content;
+}
